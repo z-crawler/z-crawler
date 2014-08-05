a967aab263e7d1fa15f0808f7c2006e21f3f0961
Thành Nguyễn Tiến <thanhnt@zigexn.vn>
2014-08-05 15:55:32 +0700

    fixed little bit

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -12,12 +12,8 @@ class PageController < ApplicationController
   end
 
   def create
-    require 'rubygems'
-    require 'nokogiri'
     page = Nokogiri::HTML(open("index.html"))
     @title = page.css('title')
-    puts @title
-    puts "============================================="
     @title_in = page.css('h1')
     @title_in_second = page.css('h2')
     @content = page.css('div').css('table').css('tbody').css('tr').css('td')
